bdf790349f037648b1e47d55b6b5deded7863712
me-hem <21f1001400@ds.study.iitm.ac.in>
2023-06-28 12:53:44 +0530

    train_test_data added

--- Detection/train_test_data.py
+++ Detection/train_test_data.py
@@ -1,3 +1,5 @@
+# Consider this file for Using data set so that we all are working on same training and test data.
+
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -6,8 +8,8 @@ random_seed = 42
 
 data = pd.read_csv("MaliciousURLDataset\MaliciousURL.csv")
 
-print(data.head)
+df = pd.DataFrame(data)
+X = df.iloc[:,0]
+y = df.iloc[:,1]
 
-X = data[0]
-y = data[1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_seed, stratify=y)
